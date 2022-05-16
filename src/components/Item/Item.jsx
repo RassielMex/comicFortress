7010dddc3cb6374c211ee6521016e95246517e06
RassielMex <rhcp94.rm@gmail.com>
2022-05-15 22:08:08 -0500

    sign added

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -8,7 +8,7 @@ const Item = ({ id, title, price, pictureUrl }) => {
       <img src={pictureUrl} className="card-img-top w-75 mt-1" alt={id} />
       <div className="card-body w-100">
         <h5 className="card-title">{title}</h5>
-        <p className="card-text">{price}</p>
+        <p className="card-text">${price}</p>
         <Link to={`/item/${id}`}>
           <button className="btn btn-warning">Ver más...</button>
         </Link>
